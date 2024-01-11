69ffa43a6d2e56a5ff04a2752101565c2f8dc101
nabu0305 <sohwi0305@gmail.com>
2024-01-12 01:31:08 +0900

    회원 서비스 개발

---
+++ src/main/java/com/Study/StudySpring/service/MemberService.java
@@ -0,0 +1,36 @@
+package com.Study.StudySpring.service;
+
+import com.Study.StudySpring.domain.Member;
+import com.Study.StudySpring.repository.MemberRepository;
+import com.Study.StudySpring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+//    회원가입
+    public  Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+            });
+    }
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
