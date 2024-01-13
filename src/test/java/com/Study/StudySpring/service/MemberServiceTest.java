7b393c79bab5db477eb4f63f6351510dde3e8570
nabu0305 <sohwi0305@gmail.com>
2024-01-13 23:36:44 +0900

    DI 설정

--- src/test/java/com/Study/StudySpring/service/MemberServiceTest.java
+++ src/test/java/com/Study/StudySpring/service/MemberServiceTest.java
@@ -3,6 +3,10 @@ package com.Study.StudySpring.service;
 import com.Study.StudySpring.domain.Member;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.Study.StudySpring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -10,9 +14,20 @@ import java.util.Optional;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
 
+    //Dependency Injection DI
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
 
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
     @Test
     void 회원가입() {
         //given
