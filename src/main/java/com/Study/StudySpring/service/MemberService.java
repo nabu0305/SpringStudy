27fc912d09d0853e199731cdd527f03904c229a7
nabu0305 <sohwi0305@gmail.com>
2024-01-14 02:21:33 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/com/Study/StudySpring/service/MemberService.java
+++ src/main/java/com/Study/StudySpring/service/MemberService.java
@@ -3,10 +3,12 @@ package com.Study.StudySpring.service;
 import com.Study.StudySpring.domain.Member;
 import com.Study.StudySpring.repository.MemberRepository;
 import com.Study.StudySpring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
