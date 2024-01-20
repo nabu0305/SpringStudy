fbddb25bf59dab23117b5141aa39e088d838e004
nabu0305 <sohwi0305@gmail.com>
2024-01-20 23:17:11 +0900

    Spring.config

--- src/main/java/com/Study/StudySpring/SpringConfig.java
+++ src/main/java/com/Study/StudySpring/SpringConfig.java
@@ -1,2 +1,22 @@
-package com.Study.StudySpring;public class SpringConfig {
+package com.Study.StudySpring;
+
+import com.Study.StudySpring.repository.MemberRepository;
+import com.Study.StudySpring.repository.MemoryMemberRepository;
+import com.Study.StudySpring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
 }
