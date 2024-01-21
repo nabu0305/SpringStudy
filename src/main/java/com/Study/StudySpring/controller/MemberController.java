f364bc7677ad31eac0883d8b1a4a064f0ccd6234
nabu0305 <sohwi0305@gmail.com>
2024-01-22 00:08:00 +0900

    회원가입 컨트롤 맵핑

--- src/main/java/com/Study/StudySpring/controller/MemberController.java
+++ src/main/java/com/Study/StudySpring/controller/MemberController.java
@@ -4,6 +4,7 @@ package com.Study.StudySpring.controller;
 import com.Study.StudySpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -13,5 +14,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
     }
 }
